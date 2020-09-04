fd3ab92040c93bbe5bfeb1cb46de44e222fef7d3
Lachlan <lachlan@lachlanb.com>
2020-09-04 14:30:09 +1000

    Reverted previous commit

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,35 +1,13 @@
 var express = require('express');
-const port = 4000;
+r();
 
-
-//var router = express.Router();
-
-const app = express();
+var router = express.Router();
 module.exports = app;
 
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
 
 /* GET home page. */
-app.get('/', function(req, res, next) {
+router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-//GET USERS
-app.get('/api/users', (req, res) => {
-  res.json({ count });
-});
-
-
-//DATABASE
-mongoose.connect('mongodb+srv://lachlan:pass@test.wdram.mongodb.net/test?retryWrites=true&w=majority',
-  {
-    useUnifiedTopology: true,
-    useNewUrlParser: true
-  }, () =>
-  console.log("Connected to DB")
-);
-
-app.listen(port, () => {
-  console.log(`API available at http://localhost:${port}/api`);
-});
+modules.exports = router;
