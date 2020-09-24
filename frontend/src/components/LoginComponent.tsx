96a7767119a5684aa520a433a18cb62d5fe1e373
Matthew Clifford <mattclifford2000@gmail.com>
2020-09-25 00:58:42 +1000

    Efforts to develop login screeb

---
+++ frontend/src/components/LoginComponent.tsx
@@ -0,0 +1,6 @@
+import * as React from "react";
+import { Login, createEmptyLogin } from '../model/Login';
+
+//export const LoginComponent: React.FC<PropsForm> = (props) => {
+
+//}
--- frontend/src/model/User.ts
+++ frontend/src/model/User.ts
@@ -7,4 +7,5 @@ export interface User extends Array<Request>{
     UserPassword: string;
     UserRequests: Request[];
     UserDOB: Date;
+    UserScore: number;
 }
