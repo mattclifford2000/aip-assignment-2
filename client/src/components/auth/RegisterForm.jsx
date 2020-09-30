d4f9ecf3fd7e84b7b1e259eb88d33995e7d9ee19
Matthew Clifford <mattclifford2000@gmail.com>
2020-09-30 17:17:59 +1000

    Fixed CSS

--- client/src/components/auth/RegisterForm.jsx
+++ client/src/components/auth/RegisterForm.jsx
@@ -1,10 +1,17 @@
-import * as React from "react";
-import Form from "react-bootstrap/Form";
-import Button from "react-bootstrap/Button";
-import "bootstrap/dist/css/bootstrap.min.css";
+import React, {Component} from "react";
+import { Button, Form, } from "react-bootstrap";
 
 
-export class RegisterComponent extends React.Component {
+export class RegisterComponent extends Component {
+  constructor(props)
+  {
+    super(props);
+    this.state = {
+      email: "",
+      password: "",
+      name: ""
+    };
+  }
   
   render() {
     return (
@@ -12,21 +19,11 @@ export class RegisterComponent extends React.Component {
         <Form>
           <Form.Group controlId="formBasicEmail">
             <Form.Label>Email Address</Form.Label>
-            <Form.Control
-              type="email"
-              name="email"
-              placeholder="Enter email"
-              
-            />
+            <Form.Control type="email" name="email" placeholder="Enter email" />
           </Form.Group>
           <Form.Group controlId="formBasicName">
             <Form.Label>Name</Form.Label>
-            <Form.Control
-              type="string"
-              name="name"
-              placeholder="Enter name"
-          
-            />
+            <Form.Control type="string" name="name" placeholder="Enter name" />
           </Form.Group>
           <Form.Group controlId="formBasicPassword">
             <Form.Label>Password</Form.Label>
@@ -34,25 +31,14 @@ export class RegisterComponent extends React.Component {
               type="password"
               name="password"
               placeholder="Password"
-     
             />
           </Form.Group>
           <Form.Group controlId="formBasicDOB">
             <Form.Label>Date of Birth</Form.Label>
-            <Form.Control
-              type="date"
-              name="dateofbirth"
-              placeholder="date"
-
-            />
+            <Form.Control type="date" name="dateofbirth" placeholder="date" />
           </Form.Group>
           <Form.Group controlId="formBasicCheckbox">
-            <Form.Check
-              name="checkbox"
-              type="checkbox"
-              label="Check me out"
-
-            />
+            <Form.Check name="checkbox" type="checkbox" label="Check me out" />
           </Form.Group>
           <Button variant="primary" type="submit">
             Submit
