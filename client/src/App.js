24fca2dbf5d9a75b27df7172d78dbb82969a7880
Matthew Clifford <mattclifford2000@gmail.com>
2020-10-10 15:58:15 +1100

    Merger

--- client/src/App.js
+++ client/src/App.js
@@ -1,55 +1,3 @@
-<<<<<<< HEAD
-import React, { useState } from "react";
-import "./App.css";
-import { BrowserRouter as Router, Link, Route } from "react-router-dom";
-import Home from "./components/pages/Home";
-import Login from "./components/pages/Login";
-import Register from "./components/pages/Register";
-import Requests from "./components/pages/Requests";
-import Profile from "./components/pages/Profile";
-import PrivateRoute from "./components/routes/PrivateRoute";
-import { AuthContext } from "./components/context/auth";
-import { Navbar, Nav } from "react-bootstrap";
-
-function App(props) {
-  const existingTokens = JSON.parse(localStorage.getItem("tokens"));
-  const [authTokens, setAuthTokens] = useState(existingTokens);
-
-  const setTokens = (data) => {
-    localStorage.setItem("tokens", JSON.stringify(data));
-    setAuthTokens(data);
-  };
-
-  return (
-    <AuthContext.Provider value={{ authTokens, setAuthTokens: setTokens }}>
-      <Router>
-        <div className="App">
-          <Navbar bg="light" expand="lg">
-            <Navbar.Brand href="#home">Favour Centre</Navbar.Brand>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
-              <Nav className="mr-auto">
-                <Nav.Link href="/">Home Page</Nav.Link>
-                <Nav.Link href="/requests">Requests</Nav.Link>
-                <Nav.Link href="/profile">Profile</Nav.Link>
-                <Nav.Link href="/register">Register</Nav.Link>
-                <Nav.Link href="/login">Login</Nav.Link>
-              </Nav>
-            </Navbar.Collapse>
-          </Navbar>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/register" component={Register} />
-          <Route exact path="/login" component={Login} />
-          <PrivateRoute exact path="/requests" component={Requests} />
-          <PrivateRoute exact path="/profile" component={Profile} />
-        </div>
-      </Router>
-    </AuthContext.Provider>
-  );
-}
-
-export default App;
-=======
 import React, { useState } from "react";
 import "./App.css";
 import { BrowserRouter as Router, Link, Route } from "react-router-dom";
@@ -120,4 +68,4 @@ function App(props) {
 }
 
 export default App;
->>>>>>> 25c778ce2d7b7fce4b611052862e3473438dbd3b
+
--- client/src/components/shared/Leaderboard.jsx
+++ client/src/components/shared/Leaderboard.jsx
@@ -1,57 +1,4 @@
-<<<<<<< HEAD
-import * as React from "react";
-import axios from "axios";
-import "./../../styles/Leaderboard.css";
 
-export default class Leaderboard extends React.Component {
-  state = {
-    users: [],
-  };
-
-  GetSortOrder(prop) {
-    return function (a, b) {
-      if (a[prop] > b[prop]) {
-        return -1;
-      } else if (a[prop] < b[prop]) {
-        return 1;
-      }
-      return 0;
-    };
-  }
-
-  componentDidMount() {
-    axios.get("/lists/leaderboard").then((res) => {
-      var users = res.data;
-      var data = [];
-      if (users.length < 10) {
-        users.sort(this.GetSortOrder("score"));
-      } else {
-        users.sort(this.GetSortOrder("score"));
-        for (var i = 0; i < 10; i++) {
-          data.push(users[i]);
-        }
-      }
-
-      this.setState({ users: data });
-    });
-  }
-
-  render() {
-    return (
-      <div>
-        <h5>Leaderboard</h5>
-        <div className="leaderboard">
-          <ol>
-            {this.state.users.map((user) => (
-              <li>{user.name + ": " + user.score + " points."}</li>
-            ))}
-          </ol>
-        </div>
-      </div>
-    );
-  }
-}
-=======
 import * as React from "react";
 import axios from "axios";
 import "./../../styles/Leaderboard.css";
@@ -106,4 +53,3 @@ export default class Leaderboard extends React.Component {
     );
   }
 }
->>>>>>> 25c778ce2d7b7fce4b611052862e3473438dbd3b
