b5e26c4d0e46b4a6ce78354814c4300bac6ab1c1
Joseph Drew <joseph.j.drew@student.uts.edu.au>
2020-10-12 17:09:47 +1100

    WIP Working on fixing token passed to jwt.verify

--- api/helpers/verifyUser.js
+++ api/helpers/verifyUser.js
@@ -5,12 +5,22 @@ const jwt = require("jsonwebtoken");
 require("dotenv").config();
 
 const verifyUser = (token) => {
+  console.log(token);
+  console.log(process.env.ACCESS_TOKEN_SECRET);
+  console.log(parseJwt(token));
   if (token == null) return { status: "401", user: {} };
-  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+  /*
+  const verified = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
     if (err) return { status: "403", user: {} };
     console.log("Valid!");
-    console.log(parseJwt(token));
   });
+  return { status: "200", user: parseJwt(token) };*/
+  try {
+    jwt.verify(token, process.env.TOKEN_SECRET);
+    //console.log(verified);
+  } catch (err) {
+    return { status: "400", user: {} };
+  }
   return { status: "200", user: parseJwt(token) };
 };
 
--- api/routes/newrequest.route.js
+++ api/routes/newrequest.route.js
@@ -6,23 +6,11 @@ const User = require("../models/User.model");
 const Request = require("../models/Request.model");
 
 router.post("/", async (req, res) => {
-  console.log("Successful POST register.");
   let verifiedUser = verifyUser(req.body.token);
-  console.log(verifiedUser.status);
   if (verifiedUser.status != "200") {
     console.log("failed");
     return res.send(verifiedUser.status);
   }
-
-  //const emailExists = await User.exists({ email: body.email });
-  /* if (emailExists)
-    return res.status(200).send("A user exists with this email.");*/
-  /*const salt = await bcrypt.genSalt(10);
-  const hashPassword = await bcrypt.hash(body.password, salt);
-  var newRole = "user";
-  if (body.email.includes("@favourcentre.com.au")) {
-    newRole = "admin";
-  }*/
   const request = new Request({
     ownerID: verifiedUser.user._id,
     requestname: req.body.requestname,
