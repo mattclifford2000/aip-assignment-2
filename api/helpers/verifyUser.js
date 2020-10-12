c2cdb115fcfe18d64f1467cdd0d199dece33bae6
Joseph Drew <joseph.j.drew@student.uts.edu.au>
2020-10-12 20:06:55 +1100

    Prior to Tidy Up
    WIP prior to tidying up files and code

--- api/helpers/verifyUser.js
+++ api/helpers/verifyUser.js
@@ -4,24 +4,17 @@ const jwt = require("jsonwebtoken");
 
 require("dotenv").config();
 
+//Returns a HTTP status code, and user if JWT is valid.
 const verifyUser = (token) => {
   console.log(token);
   console.log(process.env.ACCESS_TOKEN_SECRET);
-  //console.log(parseJwt(token));
-  if (token == null) return { status: "401", user: {} };
-  /*
-  const verified = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
-    if (err) return { status: "403", user: {} };
-    console.log("Valid!");
-  });
-  return { status: "200", user: parseJwt(token) };*/
+  if (token == null) return { status: 401, user: {} };
   try {
     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
-    //console.log(verified);
   } catch (err) {
-    return { status: "400", user: {} };
+    return { status: 400, user: {} };
   }
-  return { status: "200", user: parseJwt(token) };
+  return { status: 200, user: parseJwt(token) };
 };
 
 module.exports.verifyUser = verifyUser;
