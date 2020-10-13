88995c78bc97b5301240dcd8441b65dff3bc768b
eethanchoi <ethanchoi777@gmail.com>
2020-10-13 11:40:32 +1100

    Update request route
    Update request route

--- api/routes/request.route.js
+++ api/routes/request.route.js
@@ -3,34 +3,6 @@ var router = express.Router();
 const Request = require("../models/Request.model");
 
 
-/*
-
-router.post("/", async (req, res) => {
-
-    var requestTitle = req.body.requestTitle;
-    var cupcakes = req.body.cupcakes;
-    var chocolates = req.body.chocolates;
-    var mints = req.body.mints;
-    var coffees = req.body.coffees;
-    var icecreams = req.body.icecreams;
-
-    const request = new Request({
-        ownerID: "bob",
-        requestTitle: requestTitle,
-        requeststatus: true,
-        cupcakes: cupcakes,
-        chocolates: chocolates,
-        mints: mints,
-        coffees: coffees,
-        icecreams: icecreams
-    });
-    const savedRequest = await request.save();
-
-    console.log(savedRequest); 
-}
-
-)*/
-
 router.get("/", async (req, res) => {
   const requests = await Request.find();
   console.log(requests);
@@ -42,4 +14,32 @@ module.exports = router;
 
 
 
-/* Test */
+
+
+/*
+GET Request prior to merge with Joseph's requests
+router.post("/", async (req, res) => {
+
+    var requestTitle = req.body.requestTitle;
+    var cupcakes = req.body.cupcakes;
+    var chocolates = req.body.chocolates;
+    var mints = req.body.mints;
+    var coffees = req.body.coffees;
+    var icecreams = req.body.icecreams;
+
+    const request = new Request({
+        ownerID: "name",
+        requestTitle: requestTitle,
+        requeststatus: true,
+        cupcakes: cupcakes,
+        chocolates: chocolates,
+        mints: mints,
+        coffees: coffees,
+        icecreams: icecreams
+    });
+    const savedRequest = await request.save();
+
+    console.log(savedRequest);
+}
+
+)*/
