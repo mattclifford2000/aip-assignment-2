8943bd9874c372b7555cc297fe9784a986b2eb6d
Lachlan <lachlan@lachlanb.com>
2020-10-15 18:29:20 +1100

    .

--- app.js
+++ app.js
@@ -1,13 +1,11 @@
 var createError = require("http-errors");
 var express = require("express");
 var path = require("path");
-var http_module = require("http");
 var cookieParser = require("cookie-parser");
 var bodyParser = require("body-parser");
 var logger = require("morgan");
 var cors = require("cors");
-const jwt = require("jsonwebtoken");
-const { Base64 } = require("js-base64");
+
 
 require("dotenv").config();
 
