d5637a4ecbbdd7be86ae5bdc5fc63e0d2b14d9ab
eethanchoi <ethanchoi777@gmail.com>
2020-10-18 12:59:57 +1100

    Display request search results

--- client/src/components/pages/SearchRequests.jsx
+++ client/src/components/pages/SearchRequests.jsx
@@ -4,13 +4,18 @@ import axios from "axios";
 import { Button, Form, Card } from "react-bootstrap";
 import { Link, Redirect } from "react-router-dom";
 import "../../styles/searchRequests.css";
+import { browserHistory } from 'react-router';
 
 function SearchRequests(props) {
     const [requests, setRequests] = useState([]);
-    const [query, setQuery] = useState([]);
+    const [query, setQuery] = useState();
+    const [data, setData] = useState([]);
 
 
     function handleSubmit(e) {
+        e.preventDefault();
+
+        console.log(data)
 
         console.log(e.target.value);
         const url = "http://localhost:9000/request/searchRequest";
@@ -18,33 +23,47 @@ function SearchRequests(props) {
         axios
             .post(url, { query })
             .then((response) => {
-                setRequests(response.data)
+                setData(response.data)
             })
+
+
+
+        /*         setData([{ "name": "test1" }, { "name": "test2" }]); */
     }
 
 
-
     return (
-        <div class="searchRequestForm">
-            <h1> Search requests</h1>
-            <Card style={{ width: "18rem" }}>
-                <Form onSubmit={handleSubmit} noValidate>
-                    <Form.Group controlId="password">
-                        <Form.Control
-                            name="query"
-                            type="query"
-                            placeholder="query"
-                            value={query}
-                            onChange={(e) => {
-                                setQuery(e.target.value);
-                            }}
-                        />
-                    </Form.Group>
-                    <Button variant="primary" type="submit">
-                        Search
+        <div>
+            <div class="searchRequestForm">
+                <h1> Search requests</h1>
+                <Card style={{ width: "18rem" }}>
+                    <Form onSubmit={handleSubmit} noValidate >
+                        <Form.Group controlId="password">
+                            <Form.Control
+                                name="query"
+                                type="query"
+                                placeholder="query"
+                                value={query}
+                                onChange={(e) => {
+                                    setQuery(e.target.value);
+                                }}
+                            />
+                        </Form.Group>
+                        <Button variant="primary" type="submit">
+                            Search
           </Button>
-                </Form>
-            </Card>
+                    </Form>
+                </Card>
+            </div>
+
+            <ul>
+                {data.map((request) => (
+                    <li>
+                        {request.name}
+                        {request.content}
+                    </li>
+                ))}
+            </ul>
 
         </div>
     );
