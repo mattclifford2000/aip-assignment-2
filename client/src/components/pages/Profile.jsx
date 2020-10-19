a79bfd87a9b61bf15ec41983c2b88b8f64c4854d
eethanchoi <ethanchoi777@gmail.com>
2020-10-19 19:45:07 +1100

    Display favours the user owes and is owed on profile page

--- client/src/components/pages/Profile.jsx
+++ client/src/components/pages/Profile.jsx
@@ -8,21 +8,19 @@ function Profile(props) {
   const [requests, setRequests] = useState([]);
 
 
-  useEffect(() => {
-    axios
-      .post("/request/mine",  { authToken: localStorage.getItem('authToken') })
-      .then((res) => {
-        setRequests(res.data);
-      });
-  });
+  const [owed, setOwed] = useState([]);
+  const [owing, setOwing] = useState([]);
+
+  const [userID, setUserID] = useState(localStorage.getItem('userID'))
 
   const handleDelete = (e) => {
     e.preventDefault();
     console.log(localStorage.getItem('userID'));
-    
+
     axios
-      .post("/request/delete", { requestID: e.target.value,
-                                 authToken: localStorage.getItem('authToken')
+      .post("/request/delete", {
+        requestID: e.target.value,
+        authToken: localStorage.getItem('authToken')
       })
       .then((res) => {
         setRequests(res.data);
@@ -31,12 +29,28 @@ function Profile(props) {
 
 
   useEffect(() => {
+    /*
     axios
-      .post("/request/mine",  { authToken: localStorage.getItem('authToken') })
+      .post("/request/mine", { authToken: localStorage.getItem('authToken') })
       .then((res) => {
         setRequests(res.data);
-      });
-  }, requests);
+      }); */
+
+    const owedUrl = "http://localhost:9000/favour/myOwedFavours";
+    const owingUrl = "http://localhost:9000/favour/myOwingFavours";
+
+    axios
+      .post(owedUrl, { userID })
+      .then((response) => {
+        setOwed(response.data)
+      })
+
+    axios
+      .post(owingUrl, { userID })
+      .then((response) => {
+        setOwing(response.data)
+      })
+  }, []);
 
   return (
     <div class="center">
@@ -49,6 +63,26 @@ function Profile(props) {
           </li>
         ))}
       </ul>
+
+      <h1> Owing </h1>
+      <ol class="requestList">
+        {owed.map((favour) => (
+          <li class="request">
+            <h1> {favour.name} </h1>
+            <p>Request Description: {favour.content}</p>
+          </li>
+        ))}
+      </ol>
+      <h1> Owed </h1>
+      <ol class="requestList">
+        {owing.map((favour) => (
+          <li class="request">
+            <h1> {favour.name} </h1>
+            <p>Request Description: {favour.content}</p>
+          </li>
+        ))}
+      </ol>
+
     </div>
   );
 }
