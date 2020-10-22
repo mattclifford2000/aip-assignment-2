07cbc48504c45805fc476150a8750fc77f872ff1
Matthew Clifford <mattclifford2000@gmail.com>
2020-10-23 01:10:51 +1100

    Backend validation for request using joi

--- api/helpers/verifyRequest.js
+++ api/helpers/verifyRequest.js
@@ -2,6 +2,7 @@ const Joi = require("joi");
 Joi.objectId = require("joi-objectid")(Joi);
 
 const verifyRequest = (request) => {
+  const minAmount = -1;
   console.log("Request: ");
   console.log(request);
   const schema = Joi.object()
@@ -10,11 +11,11 @@ const verifyRequest = (request) => {
       name: Joi.string().min(6),
       content: Joi.string().min(6),
       completed: Joi.boolean(),
-      chocolates: Joi.number(),
-      mints: Joi.number(),
-      pizzas: Joi.number(),
-      coffees: Joi.number(),
-      candies: Joi.number(),
+      chocolates: Joi.number().greater(minAmount),
+      mints: Joi.number().greater(minAmount),
+      pizzas: Joi.number().greater(minAmount),
+      coffees: Joi.number().greater(minAmount),
+      candies: Joi.number().greater(minAmount),
     })
     .unknown(true);
   console.log(schema.validate(request));
