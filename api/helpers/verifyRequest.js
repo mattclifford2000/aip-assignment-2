ed8e71167ec2346f5388fea04b591a45ffce2187
Matthew Clifford <mattclifford2000@gmail.com>
2020-10-22 22:42:37 +1100

    Remove node_modules in main app folder

--- api/helpers/verifyRequest.js
+++ api/helpers/verifyRequest.js
@@ -1,21 +1,23 @@
-const Joi = require("@hapi/joi");
-Joi.objectId = require('joi-objectid')(Joi);
+const Joi = require("joi");
+Joi.objectId = require("joi-objectid")(Joi);
 
 const verifyRequest = (request) => {
   console.log("Request: ");
   console.log(request);
-  const schema = Joi.object({
-    ownerID: Joi.objectId(),
-    ownerName: Joi.string(),
-    name: Joi.string().min(6),
-    content: Joi.string().min(6),
-    completed: Joi.boolean(),
-    chocolates: Joi.number(),
-    mints: Joi.number(),
-    pizzas: Joi.number(),
-    coffees: Joi.number(),
-    candies: Joi.number(),
-  });
+  const schema = Joi.object()
+    .keys({
+      ownerID: Joi.objectId(),
+      ownerName: Joi.string(),
+      name: Joi.string().min(6),
+      content: Joi.string().min(6),
+      completed: Joi.boolean(),
+      chocolates: Joi.number(),
+      mints: Joi.number(),
+      pizzas: Joi.number(),
+      coffees: Joi.number(),
+      candies: Joi.number(),
+    })
+    .unknown(true);
   console.log(schema.validate(request));
   return schema.validate(request);
 };
