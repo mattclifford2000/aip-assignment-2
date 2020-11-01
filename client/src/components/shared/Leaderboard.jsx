4d9be4b727a90f34ac90a51cec6d32cf7a650803
Lachlan <lachlan@lachlanb.com>
2020-11-01 20:13:36 +1100

    removed unused vars

--- client/src/components/shared/Leaderboard.jsx
+++ client/src/components/shared/Leaderboard.jsx
@@ -1,4 +1,4 @@
-import React, { Component, useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from "axios";
 import "./../../styles/Leaderboard.scss";
 
